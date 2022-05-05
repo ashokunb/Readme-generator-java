a0302d3f407792d6f7086433364f4586fb0b3c1f
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-04 21:03:47 -0500

    moving branch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { type } = require('os');
+// TODO: Create an array of questions for user input
 const questions = [{
     name: "name", 
     message:"What is the name of your project title?",
@@ -10,20 +12,78 @@ const questions = [{
     name:"description",
     message:"What is the description of your project?",
     type: "input",
+},
+{
+    name:"instructions",
+    message: "What are your installation instructions:",
+    type: "input",
+},
+{
+    name:"usage",
+    message: "What are the usage information instructions:",
+    type: "input",
+},
+{
+    name:"contribution",
+    message: "What are the contribution guidelines instruction:",
+    type: "input",
+},
+{
+    name:"test",
+    message: "What are the contribution guidelines instruction:",
+    type: "input",
 }];
 
 inquirer.prompt(questions).then(answers=>{
     console.log(answers);
-    fs.writeFileSync("Readme.md", createTemplate(answers))
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+    fs.writeFileSync("Readme.md", createTemplate(answers), err => {
+        if (err) throw new Error(err);
+        console.log('Readme File is created! See your output in README.md');
+    })
 });
 
-//data is our answers
-function createTemplate(data){
-    return `# ${data.name}`
-    `#Description:`
-    `##${data.nameOne}`
-}
 
+// TODO: Create a function to initialize app
+function init(data) {
+    return `
+# ${data.name}
+/### Built with:
+- HTML
+-CSS
+-JavaScript
+-JQuery
+
+<h3>Description:</h3>
+<blockquote>
+<p>${data.description}</p>
+</blockquote>
+
+<h3>Instructions:</h3>
+<blockquote>
+${data.instructions}
+</blockquote>
+
+<h3>Usage:</h3>
+<blockquote>
+${data.usage}
+</blockquote>
+
+<h3>Contribution:</h3>
+<blockquote>
+${data.contribution}
+</blockquote>
+
+<h3>Test Instructions:</h3>
+<blockquote>
+${data.test}
+</blockquote>
+`;
+};
+
+// Function call to initialize app
+init();
 
 //What is the description of your application?
 //What are the installation instruction?
